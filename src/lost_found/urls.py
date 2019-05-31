70a5ddc337cef790e2032ab8372b57caa222e483
Zhaochun Wang <carmichael901230@gmail.com>
2019-05-30 20:00:28 -0400

    login/logout added

--- src/lost_found/urls.py
+++ src/lost_found/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from page.views import home_view
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
