8e06c8d87865e82240668b201d543ff992a7cd6a
Zhaochun Wang <carmichael901230@gmail.com>
2019-06-02 18:10:51 -0400

    login and logout

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import base_view
-from django.contrib.auth.views import LoginView
+from .views import base_view, logged_out_view
+from django.contrib.auth.views import LoginView, LogoutView
 
-# app_name = "accounts"
+app_name = "accounts"
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html')),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page="/accounts/logged_out_success"), name="logout"),
+    path('logged_out_success/', logged_out_view),
     path('', base_view),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-# def login_view(request):
-#     return render(request, 'accounts/login.html')
 
 def base_view(request):
     return render(request, 'base.html')
+
+def logged_out_view(request):
+    return render(request, 'accounts/logged_out.html')
